4589afe710692c977365931d8746ca2644f03647
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-07-10 19:25:14 +0530

    Time: 425 ms (93.07%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,17 @@
+/* Write your T-SQL query statement below */
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT
+        *,
+        DENSE_RANK() OVER (
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS SalaryRank
+    FROM Employee
+) e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.SalaryRank <= 3;
