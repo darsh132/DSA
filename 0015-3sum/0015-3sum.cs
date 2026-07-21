5a88cd235119b738415de3cd401a4f172fc43949
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-07-22 01:45:32 +0530

    Time: 35 ms (70.71%), Space: 65.7 MB (78.82%) - LeetHub

--- 0015-3sum/0015-3sum.cs
+++ 0015-3sum/0015-3sum.cs
@@ -1,16 +1,17 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
         var result = new List<IList<int>>();
+        if(nums == null || nums.Length < 3) return result;
+
+        // Step 1: Sort the array
         Array.Sort(nums);
 
         for(int i = 0; i < nums.Length - 2; i++) {
-            if(nums[i] > 0) {
-                break;
-            }
+            // Early exit: If the smallest number is greater than 0, 3 positive numbers can't sum to 0
+            if(nums[i] > 0) break;
 
-            if(i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+            // Skip duplicate values for the first element
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = nums.Length - 1;
@@ -19,24 +20,21 @@ public class Solution {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if(sum == 0) {
-                    result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                    result.Add(new List<int> {nums[i], nums[left], nums[right]});
 
-                    while(left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-
-                    while(left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
+                    // Skip duplicates for the second element
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates for the third element
+                    while(left < right && nums[right] == nums[right - 1]) right--;
 
                     left++;
                     right--;
                 }
                 else if(sum < 0) {
-                    left++;
+                    left++; // Sum is too small, increase the left boundary
                 }
                 else {
-                    right--;
+                    right--; // Sum is too large, decrease the right boundary
                 }
             }
         }
