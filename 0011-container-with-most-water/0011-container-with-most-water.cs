8d97dd7cdaaa958920f66ea1a555e7c0e4169126
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-07-22 01:18:11 +0530

    Time: 1 ms (99.59%), Space: 61.8 MB (30.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cs
+++ 0011-container-with-most-water/0011-container-with-most-water.cs
@@ -1,20 +1,20 @@
 public class Solution {
     public int MaxArea(int[] height) {
-        int maxArea = 0;
+        int maxArea =  0;
         int left = 0;
         int right = height.Length - 1;
 
-        while (left < right) {
-            // Calculate the width between the two pointers
+        while(left < right) {
+            // Calculate current width
             int width = right - left;
-            // The height of the container is limited by the shorter line
-            int currentHeight = Math.Min(height[left], height[right]);
-            // Calculate the area and update maxArea if it's larger
-            int currentArea = width * currentHeight;
 
+            // Calculate current area based on the shorter vertical line
+            int currentArea = width * Math.Min(height[left], height[right]);
+
+            // Update the maximum area seen so far
             maxArea = Math.Max(maxArea, currentArea);
 
-            // Move the pointer pointing to the shorter line
+            // Move the pointer that points to the lower line
             if(height[left] < height[right]) {
                 left++;
             } else {
