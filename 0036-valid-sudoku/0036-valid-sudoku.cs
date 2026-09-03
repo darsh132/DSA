f5f61fba2afdcfcbd04289ee512edf3cf2e2d900
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-09-03 23:58:45 +0530

    Time: 1 ms (95.95%), Space: 47.7 MB (89.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cs
+++ 0036-valid-sudoku/0036-valid-sudoku.cs
@@ -1,25 +1,27 @@
 public class Solution {
     public bool IsValidSudoku(char[][] board) {
-        HashSet<string> seen = new HashSet<string>();
-        
-        for(int r = 0; r < 9; r++)
-        {
-            for(int c = 0; c < 9; c++)
-            {
-                char num = board[r][c];
-                
-                if(num == '.')
-                    continue;
-                
-                string rowKey = num + " in row " + r;
-                string colKey = num + " in col " + c;
-                string boxKey = num + " in box " + (r / 3) + "-" + (c / 3);
-                
-                if(!seen.Add(rowKey) || !seen.Add(colKey) || !seen.Add(boxKey))
+        bool[,] rows = new bool[9,9];
+        bool[,] cols = new bool[9,9];
+        bool[,] boxes = new bool[9,9];
+
+        for(int r = 0; r < 9; r++) {
+            for(int c = 0; c < 9; c++) {
+                char ch = board[r][c];
+                if(ch == '.') continue;
+
+                int num = ch - '1'; // Map '1'-'9' to indices 0-8
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if(rows[r, num] || cols[c, num] || boxes[boxIndex, num]) {
                     return false;
+                }
+
+                rows[r, num] = true;
+                cols[c, num] = true;
+                boxes[boxIndex, num] =  true;
             }
         }
-        
+
         return true;
     }
 }
