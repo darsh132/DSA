5db28542d4f0b91632dde30b817d01d2e0533c3c
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-09-03 23:46:14 +0530

    Time: 0 ms (100%), Space: 43.3 MB (52.25%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cs
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -6,16 +6,15 @@ public class Solution {
         while(left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) {
-                return mid; // Target found
-            } else if (nums[mid] < target) {
-                left = mid + 1; // Search the right half
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
             } else {
-                right = mid - 1; // Search the left half
+                right = mid - 1;
             }
         }
 
-        // If not found, 'left' will be the correct insertion index
         return left;
     }
 }
