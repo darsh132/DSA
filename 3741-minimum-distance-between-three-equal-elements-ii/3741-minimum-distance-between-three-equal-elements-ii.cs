c1bcf69840edfe952d046c18af153afa492c4819
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-09-05 23:09:33 +0530

    Time: 124 ms (38.46%), Space: 91.5 MB (38.46%) - LeetHub

---
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MinimumDistance(int[] nums) {
+         Dictionary<int, List<int>> indices = new();
+
+        for(int i = 0; i < nums.Length; i++) {
+            if(!indices.ContainsKey(nums[i])) {
+                indices[nums[i]] = new List<int>();
+            }
+            indices[nums[i]].Add(i);
+        }
+
+        int minDistance = int.MaxValue;
+
+        foreach(var list in indices.Values) {
+            for(int i = 0; i + 2 < list.Count; i++) {
+                int dist = 2 * (list[i + 2] - list[i]);
+                minDistance = Math.Min(minDistance, dist);
+            }
+        }
+
+        return minDistance == int.MaxValue ? -1 : minDistance;
+    }
+}
