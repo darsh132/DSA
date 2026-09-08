49ec6d24abc1d9be37b674d40afa92f20f947e52
Darshan Bhikaram Bane <banedarshan007@gmail.com>
2026-09-09 00:30:34 +0530

    Time: 0 ms (100%), Space: 46.6 MB (87.87%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1; // Pointer for last valid element in nums1
+        int p2 = n - 1; // Pointer for last element in nums2
+        int p = m + n - 1; // Pointer for placing elements from the back of nums1
+
+        // Compare elements from the back and place the largest at position p
+        while(p1 >= 0 && p2 >= 0) {
+            if(nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+
+        // Copy any remaining elements from nums2
+        // (Elements remaining in nums1 are already in their correct places)
+        while(p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+    }
+}
